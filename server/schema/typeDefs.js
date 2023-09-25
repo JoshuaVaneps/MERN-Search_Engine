b8e11805ea8f379d437cc1c4427cf99d2bced5ce
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-09-25 09:38:33 -0400

    Fixed Back End

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,51 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  input BookInput {
+    title: String
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID!
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(newBook: BookInput!): User
+    removeBook(bookId: Int!): User
+  }
+`;
+
+module.exports = typeDefs;
